60115a870fdf8caa81dcfce9da9c842a91ad80fc
ekdickerson17 <79267433+ekdickerson17@users.noreply.github.com>
2022-03-22 12:36:17 -0400

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "snakegame.h"
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setStyleSheet("background-color: white;");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//TODO
+//Read the settings and parse the interface
+
+void MainWindow::on_pushButton_clicked()
+{
+   hide();
+   //Starts the game window with settings in the constructors
+   SnakeGame *w = new SnakeGame(1,2,this);
+   w->show();
+
+}
+
+
+void MainWindow::on_radioButton_clicked()
+{
+
+}
+
+void MainWindow::on_radioButton_2_clicked()
+{
+
+}
+
+void MainWindow::on_radioButton_3_clicked()
+{
+
+}
+
+void MainWindow::on_radioButton_4_clicked()
+{
+
+}
+
+void MainWindow::on_radioButton_5_clicked()
+{
+
+}
